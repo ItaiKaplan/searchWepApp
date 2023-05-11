1013fa31c06c2ccafa26a6f1121abd281a61bb64
Itai_Kaplan <itai.kaplan@wsc-sports.com>
2023-05-11 19:50:57 +0300

    Part 1

--- app.py
+++ app.py
@@ -1,43 +1,22 @@
 from flask import Flask, request, render_template, flash
 import redis
-# from flask_sqlalchemy import SQLAlchemy
-# from models import Query
-
 app = Flask(__name__)
 app.secret_key = "itai123456enlightEx$$$"
 r = redis.Redis(host='localhost', port=6379, db=0)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///queries.sqlite3'
-# app.config['SQLALCHEMY_TRAK_MODIFICATIONS'] = False
-# db = SQLAlchemy()
-# db.init_app(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
+@app.route("/home", methods=["POST", "GET"])
+@app.route("/index", methods=["POST", "GET"])
 def search():
-    if request.method == 'POST' and request.form['query'] != "":
-        query = request.form['query']
-        r.incr('queries')
-        r.lpush('searches', query)
-        flash(f'Searching for "{query}"', "info")
-    return render_template("index.html")
-
-@app.route('/stats')
-def show_stats():
-    queries = r.get('queries')
-    return f'Total searches: {queries}'
-
-# @app.route("/home", methods=["POST", "GET"])
-# @app.route("/index", methods=["POST", "GET"])
-# def home():
-#     # if request.method == 'POST':
-#     #     query = request.form['query']
-#     #     q = Query(query=query)
-#     #     db.session.add(q)
-#     #     db.session.commit()
-#     # return render_template('index.html')
+    if request.method == 'POST' and request.form['search'] != "":
+        search = request.form['search']
+        r.incr('searches_counter')
+        r.lpush('searches', search)
+        flash(f'Searching "{search}"', "info")
+    searches_counter = int(r.get('searches_counter'))
+    return render_template("index.html", num_of_queries=searches_counter)
 
 
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.create_all()
     app.run(debug=True)
--- models/Query.py
+++
@@ -1,8 +0,0 @@
-# from app import db
-#
-# class Query(db.Model):
-#     _id = db.Column(db.Integer, primary_key=True)
-#     query = db.Column(db.String, nullable=False)
-#
-#     def __init__(self, query):
-#         self.query = query
