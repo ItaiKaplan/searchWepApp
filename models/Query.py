f7441800f8ade9e0ad9fcaf15128cb1fe7f58be6
Itai_Kaplan <itai.kaplan@wsc-sports.com>
2023-05-11 10:46:30 +0300

    Basic structure

---
+++ models/Query.py
@@ -0,0 +1,8 @@
+# from app import db
+#
+# class Query(db.Model):
+#     _id = db.Column(db.Integer, primary_key=True)
+#     query = db.Column(db.String, nullable=False)
+#
+#     def __init__(self, query):
+#         self.query = query
